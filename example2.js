9ab247ab72755bed975f4a21adfddcd1e60606b0
Nellie Ann McCool <namccool@gmail.com>
2014-04-24 23:32:31 -0700

    start learnyounode

--- example2.js
+++ example2.js
@@ -1,8 +1,12 @@
 var net = require('net');
-
-var server = net.createServer(function (socket) {
-  socket.write('Echo server\r\n');
-  socket.pipe(socket);
+var server = net.createServer(function(c) { //'connection' listener
+  console.log('server connected');
+  c.on('end', function() {
+    console.log('server disconnected');
+  });
+  c.write('hello\r\n');
+  c.pipe(c);
 });
-
-server.listen(1337, '127.0.0.1');
+server.listen(8124, function() { //'listening' listener
+  console.log('server bound');
+});
